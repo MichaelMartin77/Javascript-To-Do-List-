9fe0e020396dfcdbe3d9efa0d4c2992f9bae26e0
Michael <martin.michael1477@gmail.com>
2025-03-18 22:10:22 -0600

    updated the aability to add  list items & mark them as complete through Javascript

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-const addButton = document.querySelector("button"); 
-const input = document.querySelector("#input-box"); 
+const inputBox = document.querySelector("#input-box"); 
+const listContainer = document.getElementById("list-container"); 
+const button = document.querySelector("button"); 
 
+
+
+button.addEventListener("click", (e) => {
+    if (inputBox.value === '') {
+        e.preventDefault(); 
+    }
+
+    if (inputBox.value) {
+        const li = document.createElement("li"); 
+        li.textContent = inputBox.value; 
+        listContainer.appendChild(li); 
+        inputBox.value = ''; 
+    }
+})
+
+inputBox.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const li = document.createElement("li"); 
+        li.textContent = inputBox.value; 
+        listContainer.appendChild(li); 
+        inputBox.value = ''; 
+    }
+})
+
+listContainer.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked"); 
+    }
+})
 
 
 
