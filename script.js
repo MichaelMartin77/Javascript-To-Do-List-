b4bf97ef65b49d1c318c463a517be2ac9971747f
Michael <martin.michael1477@gmail.com>
2025-03-31 14:16:33 -0600

    added a saveTask and loadTask function to save/load task on refresh

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const inputBox = document.querySelector("#input-box"); 
 const listContainer = document.getElementById("list-container"); 
 const button = document.querySelector("button"); 
-const span = document.querySelector("span"); 
 
 
 // add an click event to the button so when a user enters a list item in the input it creates the list item
 // and appends it to the list-container
 button.addEventListener("click", (e) => {
-
     if (inputBox.value.trim() === '') {
         return;
     }
@@ -17,7 +15,9 @@ button.addEventListener("click", (e) => {
         const span = document.createElement("span"); 
         span.innerHTML = "\u00d7"; 
         li.appendChild(span); 
+        saveTask();
         inputBox.value = ''; 
+        
 })
 
 // add keydown event so when a user clicks the "enter" key it creates the list item 
@@ -29,6 +29,7 @@ inputBox.addEventListener("keydown", (event) => {
         const span = document.createElement("span"); 
         span.innerHTML = "\u00d7"; 
         li.appendChild(span); 
+        saveTask();
         inputBox.value = ''; 
     }
 })
@@ -37,6 +38,7 @@ inputBox.addEventListener("keydown", (event) => {
 listContainer.addEventListener("click", (event) => {
     if (event.target.tagName === "LI") {
         event.target.classList.toggle("checked"); 
+        saveTask();
     }
 })
 
@@ -44,14 +46,46 @@ listContainer.addEventListener("click", (event) => {
     if (event.target.tagName === "SPAN") {
         const parent = event.target.parentElement; 
         parent.remove(); 
+        saveTask();
     }
 })
 
+function saveTask() {
+    const tasks = []; 
+    const listItems = listContainer.children; 
 
-span.addEventListener("click", () => {
-    console.log("You've been clicked");
-})
+    for (const item of listItems) {
+        let taskText = item.firstChild.nodeValue.trim(); 
+        let isCompleted = item.classList.contains("checked"); 
+
+        let taskObj = {
+            text: taskText, 
+            completed: isCompleted
+        }; 
+
+        tasks.push(taskObj); 
+    }
+
+    localStorage.setItem("tasks", JSON.stringify(tasks)); 
+}
 
 
+function loadTasks() {
+    const savedTask = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    for (const taskedSave of savedTask) {
+        const li = document.createElement("li"); 
+        li.textContent = taskedSave.text; 
+        if (taskedSave.completed === true) {
+            li.classList.toggle("checked"); 
+        }
+        listContainer.appendChild(li); 
+        const span = document.createElement("span"); 
+        span.innerHTML = "\u00d7"; 
+        li.appendChild(span);
+    }
+    
+}
+loadTasks(); 
 
 
